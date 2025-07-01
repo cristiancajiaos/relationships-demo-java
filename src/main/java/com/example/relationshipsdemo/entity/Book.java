7003381a2652585a20d475763f73b595ebdca858
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-01 12:25:46 -0400

    Creados Entity Book y Story - relaciones uno a uno

---
+++ src/main/java/com/example/relationshipsdemo/entity/Book.java
@@ -0,0 +1,89 @@
+package com.example.relationshipsdemo.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int bookId;
+
+  @Column(name = "book_name")
+  private String bookName;
+
+  @OneToOne(cascade = CascadeType.ALL, mappedBy = "book")
+  @JsonManagedReference
+  private Story story;
+
+  @Column(name = "created")
+  @CreationTimestamp
+  private Timestamp created;
+
+  @Column(name = "updated")
+  @UpdateTimestamp
+  private Timestamp updated;
+
+  public Book() {
+  }
+
+  public Book(int bookId, String bookName, Story story, Timestamp created, Timestamp updated) {
+    this.bookId = bookId;
+    this.bookName = bookName;
+    this.story = story;
+    this.created = created;
+    this.updated = updated;
+  }
+
+  public int getBookId() {
+    return bookId;
+  }
+
+  public void setBookId(int bookId) {
+    this.bookId = bookId;
+  }
+
+  public String getBookName() {
+    return bookName;
+  }
+
+  public void setBookName(String bookName) {
+    this.bookName = bookName;
+  }
+
+  public Story getStory() {
+    return story;
+  }
+
+  public void setStory(Story story) {
+    this.story = story;
+  }
+
+  public Timestamp getCreated() {
+    return created;
+  }
+
+  public void setCreated(Timestamp created) {
+    this.created = created;
+  }
+
+  public Timestamp getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(Timestamp updated) {
+    this.updated = updated;
+  }
+}
---
+++ src/main/java/com/example/relationshipsdemo/entity/Story.java
@@ -0,0 +1,92 @@
+package com.example.relationshipsdemo.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "story")
+public class Story {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int storyId;
+
+  @Column(name = "story_name")
+  private String storyName;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "book_id")
+  @JsonBackReference
+  private Book book;
+
+  @Column(name = "created")
+  @CreationTimestamp
+  private Timestamp created;
+
+  @Column(name = "updated")
+  @UpdateTimestamp
+  private Timestamp updated;
+
+  public Story() {
+  }
+
+  public Story(int storyId, String storyName, Book book, Timestamp created,
+      Timestamp updated) {
+    this.storyId = storyId;
+    this.storyName = storyName;
+    this.book = book;
+    this.created = created;
+    this.updated = updated;
+  }
+
+  public int getStoryId() {
+    return storyId;
+  }
+
+  public void setStoryId(int storyId) {
+    this.storyId = storyId;
+  }
+
+  public String getStoryName() {
+    return storyName;
+  }
+
+  public void setStoryName(String storyName) {
+    this.storyName = storyName;
+  }
+
+  public Book getBook() {
+    return book;
+  }
+
+  public void setBook(Book book) {
+    this.book = book;
+  }
+
+  public Timestamp getCreated() {
+    return created;
+  }
+
+  public void setCreated(Timestamp created) {
+    this.created = created;
+  }
+
+  public Timestamp getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(Timestamp updated) {
+    this.updated = updated;
+  }
+}
